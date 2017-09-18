70a858de8854f5e43a0dd1908c36aa586ea29d41
Denis Balobanov <d.balobanov@aimprosoft.com>
2017-09-18 18:28:12 +0300

    Back model, repository, controller

---
+++ server-bank/src/main/java/com/balobanov/controllers/BankController.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.balobanov.controllers;
+
+import com.balobanov.models.Bank;
+import com.balobanov.repositories.BankRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BankController {
+
+    private final BankRepositories bankRepositories;
+
+    @Autowired
+    public BankController(BankRepositories bankRepositories) {
+        this.bankRepositories = bankRepositories;
+    }
+
+    @RequestMapping(value = "banks", method = RequestMethod.GET)
+    public List<Bank> all(){
+       return bankRepositories.findAll();
+    }
+
+    @RequestMapping(value = "banks", method = RequestMethod.POST, consumes = "application/json")
+    public Bank save(@RequestBody Bank bank){
+        return bankRepositories.save(bank);
+    }
+
+    @RequestMapping(value = "banks", method = RequestMethod.PUT, consumes = "application/json")
+    public Bank update(@RequestBody Bank bank){
+        return bankRepositories.save(bank);
+    }
+
+    @RequestMapping(value = "banks", method = RequestMethod.DELETE, consumes = "application/json")
+    public Bank delete(@RequestBody Bank bank){
+        bankRepositories.delete(bank);
+        return bank;
+    }
+}
--- server-bank/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-insert into user(id, name, login, password) values (1,'Den','den','den');
-insert into role(id, name) values (1,'ROLE_USER');
-
-insert into user_role(user_id, role_id) values (1,1);
