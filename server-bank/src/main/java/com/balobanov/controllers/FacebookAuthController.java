333612f2f22846fccaee07a1f671f265e707f92d
Denis Balobanov <d.balobanov@aimprosoft.com>
2017-11-14 15:44:31 +0200

    backend get user info via restTemplate

--- server-bank/src/main/java/com/balobanov/controllers/FacebookAuthController.java
+++ server-bank/src/main/java/com/balobanov/controllers/FacebookAuthController.java
@@ -1,14 +1,28 @@
 package com.balobanov.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.LinkedHashMap;
 
 @RestController
 @RequestMapping(value = "facebook")
 public class FacebookAuthController {
 
+    private RestTemplate restTemplate;
+
     @RequestMapping(method = RequestMethod.POST)
-    public void authPost(){
+    public LinkedHashMap authGetUser(@RequestParam String accessToken){
+        String s = "https://graph.facebook.com/me?access_token={#token}&fields=about, name, email, picture, birthday, gender, last_name, first_name, middle_name";
+        return restTemplate.getForObject(s.replace("{#token}", accessToken), LinkedHashMap.class);
+    }
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 }
