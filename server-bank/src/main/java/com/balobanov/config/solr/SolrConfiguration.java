5f9c3356a6cb8484e09d296580f4078105c5f244
Denis Balobanov <d.balobanov@aimprosoft.com>
2017-11-29 18:14:06 +0200

    solr

---
+++ server-bank/src/main/java/com/balobanov/config/solr/SolrConfiguration.java
@@ -0,0 +1,25 @@
+package com.balobanov.config.solr;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.repository.config.EnableSolrRepositories;
+
+@Configuration
+@ComponentScan({"com.balobanov.models.solr"})
+@EnableSolrRepositories(basePackages = "com.balobanov.repositories.solr")
+public class SolrConfiguration {
+
+    @Bean
+    public SolrClient solrClient() {
+        return new HttpSolrClient("http://localhost:8983/solr/mycol1");
+    }
+
+    @Bean
+    public SolrTemplate solrTemplate(SolrClient client) throws Exception {
+        return new SolrTemplate(client);
+    }
+}
