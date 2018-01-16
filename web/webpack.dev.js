1f976130389dd45fd57f37e4383ed142ecb5f4bd
d.balobanov <d.balobanov@aimprosoft.com>
2018-01-16 16:57:21 +0200

    webpack diff configs

---
+++ web/webpack.dev.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const VENDOR_LIBS = [
+    'axios', 'react', 'lodash', 'redux', 'react-redux', 'react-dom',
+    'faker', 'react-input-range', 'redux-form', 'redux-thunk'
+];
+
+module.exports = {
+    entry: {
+        bundle: './src/app.js',
+        vendor: VENDOR_LIBS
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[hash].js'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        }),
+        new ExtractTextPlugin("styles.css"),
+        new HtmlWebpackPlugin({
+            template: 'src/html/index.html'
+        })
+    ],
+    devServer: {
+        // contentBase: path.join(__dirname, "dist"),
+        // compress: false,
+        port: 9000,
+        proxy:{
+            "/**": {
+                target: "http://localhost:8080",
+            }
+        },
+    },
+
+    devtool: 'cheap-module-eval-source-map'
+};
